4d89094f96cba7c55a3c39d73969c4be62875693
Timothée Couble <timothee.couble@gmail.com>
2021-02-10 16:15:52 +0100

    Remove comment

--- server/app_config.py
+++ server/app_config.py
@@ -11,15 +11,7 @@ DEBUG = True
 
 # Application (client) ID of app registration
 CLIENT_ID = config['microsoft_api']['client_id']
-
-# Placeholder - for use ONLY during testing.
 CLIENT_SECRET = config['microsoft_api']['client_secret']
-# In a production app, we recommend you use a more secure method of storing your secret,
-# like Azure Key Vault. Or, use an environment variable as described in Flask's documentation:
-# https://flask.palletsprojects.com/en/1.1.x/config/#configuring-from-environment-variables
-# CLIENT_SECRET = os.getenv("CLIENT_SECRET")
-# if not CLIENT_SECRET:
-#     raise ValueError("Need to define CLIENT_SECRET environment variable")
 
 AUTHORITY = "https://login.microsoftonline.com/common"  # For multi-tenant app
 # AUTHORITY = "https://login.microsoftonline.com/Enter_the_Tenant_Name_Here"
