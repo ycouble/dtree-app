22ee66b5fc7fc6137601336b2754098aa108efe7
Timothée Couble <timothee.couble@gmail.com>
2021-02-10 16:13:29 +0100

    Update creadentials

--- server/app_config.py
+++ server/app_config.py
@@ -1,4 +1,8 @@
 import os
+import configparser
+
+config = configparser.ConfigParser()
+config.read('.credentials.ini')
 
 # Application port
 PORT = 5000
@@ -6,10 +10,10 @@ PORT = 5000
 DEBUG = True
 
 # Application (client) ID of app registration
-CLIENT_ID = "06b0e518-e191-4997-a88b-1a3972e53928"
+CLIENT_ID = config['microsoft_api']['client_id']
 
 # Placeholder - for use ONLY during testing.
-CLIENT_SECRET = "L4.N~94_yTc~KqTd4BH.07.DJXAFsUmuN5"
+CLIENT_SECRET = config['microsoft_api']['client_secret']
 # In a production app, we recommend you use a more secure method of storing your secret,
 # like Azure Key Vault. Or, use an environment variable as described in Flask's documentation:
 # https://flask.palletsprojects.com/en/1.1.x/config/#configuring-from-environment-variables
@@ -22,11 +26,11 @@ AUTHORITY = "https://login.microsoftonline.com/common"  # For multi-tenant app
 
 # You can find the proper permission names from this document
 # https://docs.microsoft.com/en-us/graph/permissions-reference
-SCOPE = ["User.ReadBasic.All"]
+SCOPE = ["User.Read"]
 
 # Specifies the token cache should be stored in server-side session
 SESSION_TYPE = "filesystem"
-SECRET_KEY = "changeme"
+SECRET_KEY = config['dtree_app']['secret_key']
 
 # Set-up Cors headers
 CORS_HEADERS = 'Content-Type'
